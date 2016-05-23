1193f544c519a5ed4b347145f85fcbe27f4ed28d
Mohamed Saleem <mohsal.saleem@gmail.com>
2016-05-23 16:18:05 +0530

    Added index and changed port

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var router = require('./app/routes/router');
 
+app.set('port', (process.env.PORT || 8080));
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 router(app);
 
-app.listen(8080, function() {
-	console.log('Node server listening on 8080');
+app.listen(app.get('port'), function() {
+	console.log('Node server listening on', app.get('port'));
 })
